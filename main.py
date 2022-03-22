931cfa67ee3eb7391fb9e5a379529a195350fd81
Konstantin Beloborodov <kosta.white@gmail.com>
2022-03-22 11:41:31 +0300

    get employees list who in bank vault now

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
+import datetime
 import os
 
 import django
+from django.utils import timezone
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 django.setup()
 
-from datacenter.models import Passcard  # noqa: E402
 from datacenter.models import Visit
 
 if __name__ == '__main__':
     visits = Visit.objects.filter(leaved_at__isnull=True)
-    print(*visits, sep='\n')
+    for visit in visits:
+        print(visit.passcard.owner_name)
+        leaved_at = timezone.localtime()
+        entered_at = timezone.localtime(visit.entered_at)
+        time_delta = (leaved_at - entered_at)
+        print(f'Зашёл в хранилище, время по Москве:\n'
+              f'{entered_at}\n'
+              f'Находится в хранилище:\n'
+              f'{time_delta}\n')
