3179ce93b83c9138e395b42fd0fa2251a2d440e0
Konstantin Beloborodov <kosta.white@gmail.com>
2022-03-22 06:30:30 +0300

    get and print values from one passcard

--- main.py
+++ main.py
@@ -8,5 +8,9 @@ django.setup()
 from datacenter.models import Passcard  # noqa: E402
 
 if __name__ == '__main__':
-    # Программируем здесь
-    print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
+    # print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
+    passcards = Passcard.objects.all().values()
+    print(f'owner_name:\t{passcards[0]["owner_name"]}\n'
+          f'passcode:\t{passcards[0]["passcode"]}\n'
+          f'created_at:\t{passcards[0]["created_at"]}\n'
+          f'is_active:\t{passcards[0]["is_active"]}')
