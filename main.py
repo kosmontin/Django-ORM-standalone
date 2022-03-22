26441e3a29107aa8d3ee47690131f87f80c713d9
Konstantin Beloborodov <kosta.white@gmail.com>
2022-03-22 07:56:49 +0300

    get summary for all and active passcards, lite version

--- main.py
+++ main.py
@@ -9,9 +9,6 @@ from datacenter.models import Passcard  # noqa: E402
 
 if __name__ == '__main__':
     passcards = Passcard.objects.all()
-    active_passcards = []
-    for passcard in passcards.values():
-        if passcard['is_active']:
-            active_passcards.append(passcard)
+    active_passcards = Passcard.objects.filter(is_active=True)
     print('Всего пропусков', passcards.count())
     print('Активных пропусков', len(active_passcards))
